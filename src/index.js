656244d3b2345bc3ba881f2458f1d6cc2efd7f0d
man.yuan <man.yuan@zhangmen.com>
2019-10-17 17:36:34 +0800

    'update'

--- src/index.js
+++ src/index.js
@@ -3,6 +3,72 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { registerMicroApps, runDefaultMountEffects, start } from 'qiankun';
+import Framework from './Framework';
+// import Framework from './Framework.vue';
+
+// let app = null;
+
+function render({ appContent, loading }) {
+
+  /*
+  examples for vue
+   */
+  // if (!app) {
+  //   app = new Vue({
+  //     el: '#container',
+  //     data() {
+  //       return {
+  //         content: appContent,
+  //         loading,
+  //       };
+  //     },
+  //     render(h) {
+  //       return h(Framework, {
+  //         props: {
+  //           content: this.content,
+  //           loading: this.loading,
+  //         },
+  //       });
+  //     },
+  //   });
+  // } else {
+  //   app.content = appContent;
+  //   app.loading = loading;
+  // }
+
+  const container = document.getElementById('container');
+  ReactDOM.render(<Framework loading={loading} content={appContent}/>, container);
+}
+
+function genActiveRule(routerPrefix) {
+  return location => location.pathname.startsWith(routerPrefix);
+}
+
+render({ loading: true });
+
+registerMicroApps(
+  [
+    { name: 'react app', entry: 'http://localhost:7100', render, activeRule: genActiveRule('/react') },
+    { name: 'react15 app', entry: '//localhost:7102', render, activeRule: genActiveRule('/15react15') },
+    { name: 'vue app', entry: '//localhost:7101', render, activeRule: genActiveRule('/vue') },
+  ],
+  {
+    beforeLoad: [app => {
+      console.log('before load', app);
+    }],
+    beforeMount: [app => {
+      console.log('before mount', app);
+    }],
+    afterUnmount: [app => {
+      console.log('after unload', app);
+    }],
+  },
+);
+
+runDefaultMountEffects('/react');
+
+start();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
